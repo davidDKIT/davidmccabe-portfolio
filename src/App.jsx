26e9c3693f6ff9418b9ef20d39d24977e6271a59
David McCabe <93914101+davidDKIT@users.noreply.github.com>
2024-08-27 19:31:38 +0100

    :sparkles: Created skills section boiler plate and loaded through app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Projects from './sections/Projects/Projects'
 import Welcome from './sections/Welcome/Welcome'
+import Skills from './sections/Skills/Skills'
 
 function App() {
   return <>
   <Welcome/>
   <Projects/>
+  <Skills/>
   </>
 }
 
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import styles from './SkillsStyles.module.css';
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1>My Skills</h1>
+    </section>
+  );
+}
+
+export default Skills;
